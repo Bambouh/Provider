9c412d0b07c23f16b22f5a3aea2617ae85b8a1a3
David Wasa <david.wasa@gmail.com>
2011-09-28 09:11:36 +0200

    Importation de Wasa Util

---
+++ WasaUtil/src/wasa/util/IUniqueDateFactory.java
@@ -0,0 +1,16 @@
+package wasa.util;
+
+
+import java.util.Date;
+
+/**
+ * Garanties that all calls to this factory returns a unique date.
+ * Multithread ready. 
+ * Unique through all the instances. 
+ * To make it unique, it adds n millisecondes to the provided date,
+ * until the date gets unique.
+ */
+public interface IUniqueDateFactory {
+	
+	Date build(Date nonUniqueDate);
+}
